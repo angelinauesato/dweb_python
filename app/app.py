9d1df765921d9d0f1fe775b6757829bbe126d3d9
Rafael de Paiva Rogerio <helladarion@gmail.com>
2024-01-21 16:03:00 -0700

    Adding templates, using bootstrap and a navbar, also using a layout as a starting point to all other pages, some changes in app.py to use the new templates.

--- app/app.py
+++ app/app.py
@@ -6,23 +6,24 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 def execute_shell_script():
-	ssh.connect('rdepaiva.tplinkdns.com', username='helladarion', key_filename='/sshkey/id_ed25519_web_patch')
-	stdin, stdout, stderr = ssh.exec_command('./script.sh')
+    ssh.connect('rdepaiva.tplinkdns.com', username='helladarion', key_filename='/sshkey/id_ed25519_web_patch')
+    stdin, stdout, stderr = ssh.exec_command('./script.sh')
 
-	output = stdout.read().decode('utf-8')
-	error = stderr.read().decode('utf-8')
+    output = stdout.read().decode('utf-8')
+    error = stderr.read().decode('utf-8')
 
-	ssh.close()
-	return output, error
+    ssh.close()
+    return output, error
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	output, err = execute_shell_script()
-	return f"Hello Rafa!<pre> {output}</pre> <br />Errors:<br /> <pre> {err} </pre>"
+    output, err = execute_shell_script()
+    return render_template('home.html', title="Web Shell Control", output=output, err=err)
+#return f"Hello Rafa!<pre> {output}</pre> <br />Errors:<br /> <pre> {err} </pre>"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
